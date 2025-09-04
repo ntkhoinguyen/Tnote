763516e30b7d74940cd631dee07e207224a7db87
khoinguyen <khoinguyennguyen@hhdgroup.com>
2025-09-04 11:42:40 +0700

    setup test

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleNameMapper: {
+    "^__mocks__/(.*)$": "<rootDir>/__mocks__/$1",
+  },
+  preset: "jest-expo",
+};
---
+++ setupTests.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-undef
+jest.mock("@react-native-async-storage/async-storage", () =>
+    require("@react-native-async-storage/async-storage/jest/async-storage-mock")
+  );
+  
+  // eslint-disable-next-line no-undef
+  jest.mock("react-native-reanimated", () =>
+    require("react-native-reanimated/mock")
+  );
+  
+  // eslint-disable-next-line no-undef
+  jest.mock("@expo/vector-icons", () => {
+    const View = require("react-native").View;
+    return {
+      MaterialIcons: (props) => <View {...props} />,
+      FontAwesome: (props) => <View {...props} />,
+      Ionicons: (props) => <View {...props} />,
+      FontAwesome5: (props) => <View {...props} />,
+      FontAwesome6: (props) => <View {...props} />,
+      MaterialCommunityIcons: (props) => <View {...props} />,
+      AntDesign: (props) => <View {...props} />,
+      Octicons: (props) => <View {...props} />,
+      SimpleLineIcons: (props) => <View {...props} />,
+      EvilIcons: (props) => <View {...props} />,
+      Feather: (props) => <View {...props} />,
+    };
+  });
